83500c747b71a6af99304424d9d7247e763eadb6
Kang Soon Ang <ks_0520@hotmail.com>
2016-02-19 04:20:42 +0800

    redone GUI layout, created basic menu items

--- src/urgenda/gui/MainController.java
+++ src/urgenda/gui/MainController.java
@@ -1,27 +1,51 @@
 package urgenda.gui;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.FlowPane;
+
+
 public class MainController {
+	
+	public KeyCombination keyUndo = new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_DOWN);
+	public KeyCombination keyRedo = new KeyCodeCombination(KeyCode.Y, KeyCombination.CONTROL_DOWN);
 	
 	@FXML
 	private TextField inputBar;
-	@FXML
-	private TextArea feedbackField;
-	@FXML
-	private FlowPane display;
 	
+	//tester method for detecting input in inputBar
 	public void commandListener(KeyEvent event) {
 		KeyCode code = event.getCode();
 		if(code == KeyCode.ENTER) {
-			testCommand(inputBar.getText());
+			inputBar.setText(("entered:" + inputBar.getText()));
 		}
 	}
-	public void testCommand(String command) {
-		feedbackField.setText(command);
+	public void executeUndo(ActionEvent e){
+		//run undo method
+		inputBar.setText("undo!");
 	}
+	
+	public void executeRedo(ActionEvent e){
+		//run redo method
+		inputBar.setText("redo!");
+	}
+	
+	public void openHelp(ActionEvent e) {
+		//open help menu
+		inputBar.setText("help!");
+	}
+	
+	public void exitByMenu(ActionEvent e){
+		//save all edits
+		Platform.exit();
+        System.exit(0);
+	}
+	
+	//TODO: create resize method for rounded rect and display anchor panel
+	
 }
