d61db9ca4298148cbe01441d60e47e69082db11a
Tan Hui Kee Joanne <A0127358@u.nus.edu>
2016-02-23 19:20:14 +0800

    some edits of logic class

--- src/urgenda/logic/Logic.java
+++ src/urgenda/logic/Logic.java
@@ -1,5 +1,25 @@
 package urgenda.logic;
 
+import urgenda.command.Command;
+import urgenda.parser.Parser;
+
 public class Logic {
 
+	private static final String MESSAGE_WELCOME = "Welcome to Urgenda. Your task manager is ready for use.";
+
+	private LogicData _logicData; //attributes of logic?? may include more as we code
+	
+	public Logic() {
+		_logicData = new LogicData();//constructor of logic data, every new launch will empty data in logic data
+	}
+	
+	public static String launchProg() {
+		_logicData.retrieveData();
+		return MESSAGE_WELCOME;	
+	}
+	
+	public static String executeCommand(String command) {
+		Command currCmd = Parser.parseCommand(command); //parser take in a string and return it in its corresponding class obj
+		return currCmd.execute();
+	}
 }
