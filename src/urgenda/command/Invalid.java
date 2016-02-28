655acd1e23e4220e40069fad5e9faff418a06064
KhaiVo <khai.voh@gmail.com>
2016-02-28 19:47:11 +0800

    Added files via upload
    Invalid command type

---
+++ src/urgenda/command/Invalid.java
@@ -0,0 +1,14 @@
+package urgenda.parser;
+
+import urgenda.command.Command;
+import urgenda.logic.LogicData;
+
+public class Invalid implements Command {
+
+	@Override
+	public String execute(LogicData data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
