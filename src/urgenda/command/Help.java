4fc39dbcd5b1b5666af9ee182b55972a3ce815ab
Tan Hui Kee Joanne <A0127358@u.nus.edu>
2016-02-28 21:57:46 +0800

    added new command class help

---
+++ src/urgenda/command/Help.java
@@ -0,0 +1,13 @@
+package urgenda.command;
+
+import urgenda.logic.LogicData;
+
+public class Help implements Command {
+
+	@Override
+	public String execute(LogicData data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
