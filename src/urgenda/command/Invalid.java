d518f8ee7f09ee894ce8919e6fb81b41a996d561
Koh Kim Wee <a0080436@u.nus.edu>
2016-03-07 14:02:27 +0800

    added implementation for exit and invalid

--- src/urgenda/command/Invalid.java
+++ src/urgenda/command/Invalid.java
@@ -4,11 +4,27 @@ import urgenda.command.Command;
 import urgenda.logic.LogicData;
 
 public class Invalid implements Command {
+	
+	private static final String MESSAGE_INVALID_COMMAND = "\"%1$s\" is not a valid command";
+	
+	private String _command;
+	
+	public Invalid() {
+		
+	}
+	
+	public Invalid(String command) {
+		_command = command;
+	}
 
 	@Override
 	public String execute(LogicData data) {
-		// TODO Auto-generated method stub
-		return null;
+		data.setCurrState(LogicData.DisplayState.INVALID_COMMAND);
+		return String.format(MESSAGE_INVALID_COMMAND, _command);
+	}
+	
+	public void setCommand(String command) {
+		_command = command;
 	}
 
 }
--- src/urgenda/logic/LogicData.java
+++ src/urgenda/logic/LogicData.java
@@ -19,7 +19,7 @@ import urgenda.util.Task;
 public class LogicData {
 
 	public enum DisplayState {
-		ALL_TASKS, MULTIPLE_DELETE, MULTIPLE_COMPLETE, MULTIPLE_PRIORITISE, SHOW_SEARCH
+		ALL_TASKS, MULTIPLE_DELETE, MULTIPLE_COMPLETE, MULTIPLE_PRIORITISE, SHOW_SEARCH, EXIT, INVALID_COMMAND
 	}
 
 	private static final String MESSAGE_EMPTY_UNDO = "Nothing to undo";
@@ -68,7 +68,7 @@ public class LogicData {
 
 	// TODO: refactor function
 	public StateFeedback getState() {
-		StateFeedback state;
+		StateFeedback state = null;
 		switch (_currState) {
 			case ALL_TASKS :
 				updateState();
@@ -81,6 +81,14 @@ public class LogicData {
 			case SHOW_SEARCH :
 				state = displayAllTasks(_displays);
 				state.setState(StateFeedback.State.MULTIPLE_MATCHES);
+				break;
+			case EXIT :
+				saveContents();
+				System.exit(0);
+				break;
+			case INVALID_COMMAND :
+				state = displayAllTasks(_tasks);
+				state.setState(StateFeedback.State.ERROR);
 				break;
 			default :
 				state = displayAllTasks(_tasks);
