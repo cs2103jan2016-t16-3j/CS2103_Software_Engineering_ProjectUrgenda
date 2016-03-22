05d6c165a584cc0b6718d71aa98498d0f582a851
Koh Kim Wee <a0080436@u.nus.edu>
2016-03-22 22:43:59 +0800

    added sorting for multiple blocks initialisation

--- src/urgenda/util/MultipleSlot.java
+++ src/urgenda/util/MultipleSlot.java
@@ -2,6 +2,9 @@ package urgenda.util;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
+
+import org.ocpsoft.prettytime.shade.edu.emory.mathcs.backport.java.util.Collections;
 
 // MultipleSlot is an attribute for Tasks with multiple possible timings. Main timing displayed is shown in 
 // _startTime and _endTime for Task. Alternatives that are later will be placed as multiple slots to be confirmed
@@ -21,9 +24,20 @@ public class MultipleSlot {
 		return _slots;
 	}
 	
+	// TODO sort for parser's return
 	public void sortSlots() {
-		// TODO SORT
+		Collections.sort(_slots, comparator);
 	}
+	
+	static Comparator<TimePair> comparator = new Comparator<TimePair>() {
+		public int compare(final TimePair p1, final TimePair p2) {
+			if (p1.getStart().equals(p2.getStart())) {
+				return p1.getEnd().compareTo(p2.getEnd());
+			} else {
+				return p1.getStart().compareTo(p2.getStart());
+			}
+		}
+	};
 	
 	public TimePair getNextSlot() {
 		if (_slots.isEmpty()) {
