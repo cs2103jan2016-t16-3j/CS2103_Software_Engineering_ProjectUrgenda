7392faab374791c464fdba6e302df3361a1ebe1f
Cheng Tze Jin <elbatius@gmail.com>
2016-03-25 15:44:16 +0800

    fixed tasktest

--- src/test/testUtils/TaskTest.java
+++ src/test/testUtils/TaskTest.java
@@ -42,15 +42,16 @@ public class TaskTest {
 	//This fn is for checking whether a task has been marked as important
 	//test cases partition into 2 main types: yes or no, 6 subtypes: floating yes,no ; event yes,no; deadline yes,no
 	//also test empty task
+	@Test
 	public void testIsImportant() {
 		ArrayList<String> _tags = new ArrayList<String>();
 		LocalDateTime notime = null;
 		MultipleSlot slot = null;
 		Task obj = new Task(1, "Buy milk", "floating", "", false, false, false, notime, notime, LocalDateTime.now(), notime, _tags, slot);
-		Task obj2 = new Task(2, "Submit ie2150 draft", "deadline", "", true, false, true, notime, LocalDateTime.of(2016, Month.FEBRUARY, 24, 23, 59), LocalDateTime.now(), notime, _tags, slot);
-		Task obj3 = new Task(3, "Submit ie2100 hw3", "deadline", "", false, true, false, notime, LocalDateTime.now(), LocalDateTime.now(), notime, _tags, slot);
-		Task obj4 = new Task(4, "Dental Appointment", "event", " ", true, false, false, LocalDateTime.now().minusHours(3), LocalDateTime.now().plusHours(1), LocalDateTime.now(), notime,  _tags, slot);
-		Task obj5 = new Task(5, "Travel to Sweden", "event", " ", false, true, false, LocalDateTime.of(2016, Month.JULY, 26, 00, 00), LocalDateTime.of(2016, Month.AUGUST, 17, 23, 59), LocalDateTime.now(), notime,  _tags, slot);
+		Task obj2 = new Task(2, "Submit ie2150 draft", "deadline", "", true, true, true, notime, LocalDateTime.of(2016, Month.FEBRUARY, 24, 23, 59), LocalDateTime.now(), notime, _tags, slot);
+		Task obj3 = new Task(3, "Submit ie2100 hw3", "deadline", "", false, false, false, notime, LocalDateTime.now(), LocalDateTime.now(), notime, _tags, slot);
+		Task obj4 = new Task(4, "Dental Appointment", "event", " ", true, true, false, LocalDateTime.now().minusHours(3), LocalDateTime.now().plusHours(1), LocalDateTime.now(), notime,  _tags, slot);
+		Task obj5 = new Task(5, "Travel to Sweden", "event", " ", false, false, false, LocalDateTime.of(2016, Month.JULY, 26, 00, 00), LocalDateTime.of(2016, Month.AUGUST, 17, 23, 59), LocalDateTime.now(), notime,  _tags, slot);
 		Task obj6 = new Task(1, "Renew pastport", "floating", "", true, true, false, notime, notime, LocalDateTime.now(), notime, _tags, slot);
 		Task obj7 = new Task();
 		
@@ -65,6 +66,7 @@ public class TaskTest {
 	
 	//This fn is for checking whether a task is overdue
 	//test cases partition into 2 main types: yes or no; deadline can be yes or no, floating, event and empty will always be no
+	@Test
 	public void testIsOverdue() {
 		ArrayList<String> _tags = new ArrayList<String>();
 		LocalDateTime notime = null;
@@ -90,24 +92,25 @@ public class TaskTest {
 	//overlap: task tested same as compared task
 	//overlap: task tested lies within compared task
 	//overlap: test compared is subset of test tested
+	@Test
 	public void testHasOverlap() {
 		Task test = new Task();
-		LocalDateTime start = LocalDateTime.of(2016, Month.MARCH, 9, 00, 00);
-		LocalDateTime end = LocalDateTime.of(2016, Month.MARCH, 22, 00, 00);
-		LocalDateTime comparestart1 = LocalDateTime.of(2016, Month.MARCH, 9, 00, 00);
-		LocalDateTime compareend1 = LocalDateTime.of(2016, Month.MARCH, 22, 00, 00);
-		LocalDateTime comparestart2 = LocalDateTime.of(2016, Month.MARCH, 19, 30, 00);
-		LocalDateTime compareend2 = LocalDateTime.of(2016, Month.MARCH, 20, 45, 00);
-		LocalDateTime comparestart3 = LocalDateTime.of(2016, Month.MARCH, 7, 15, 00);
-		LocalDateTime compareend3 = LocalDateTime.of(2016, Month.MARCH, 11, 17, 00);
-		LocalDateTime comparestart4 = LocalDateTime.of(2016, Month.MARCH, 12, 35, 00);
-		LocalDateTime compareend4 = LocalDateTime.of(2016, Month.MARCH, 23, 30, 00);
-		LocalDateTime comparestart5 = LocalDateTime.of(2016, Month.MARCH, 7, 00, 00);
-		LocalDateTime compareend5 = LocalDateTime.of(2016, Month.MARCH, 9, 00, 00);
-		LocalDateTime comparestart6 = LocalDateTime.of(2016, Month.MARCH, 22, 00, 00);
-		LocalDateTime compareend6 = LocalDateTime.of(2016, Month.MARCH, 23, 00, 00);
-		LocalDateTime comparestart7 = LocalDateTime.of(2016, Month.MARCH, 8, 00, 00);
-		LocalDateTime compareend7 = LocalDateTime.of(2016, Month.MARCH, 23, 00, 00);
+		LocalDateTime start = LocalDateTime.of(2016, Month.MARCH, 1, 9, 00, 00);
+		LocalDateTime end = LocalDateTime.of(2016, Month.MARCH, 1, 22, 00, 00);
+		LocalDateTime comparestart1 = LocalDateTime.of(2016, Month.MARCH, 1, 9, 00, 00);
+		LocalDateTime compareend1 = LocalDateTime.of(2016, Month.MARCH, 1, 22, 00, 00);
+		LocalDateTime comparestart2 = LocalDateTime.of(2016, Month.MARCH, 1, 19, 30, 00);
+		LocalDateTime compareend2 = LocalDateTime.of(2016, Month.MARCH, 1, 20, 45, 00);
+		LocalDateTime comparestart3 = LocalDateTime.of(2016, Month.MARCH, 1, 7, 15, 00);
+		LocalDateTime compareend3 = LocalDateTime.of(2016, Month.MARCH, 1, 11, 17, 00);
+		LocalDateTime comparestart4 = LocalDateTime.of(2016, Month.MARCH, 1, 12, 35, 00);
+		LocalDateTime compareend4 = LocalDateTime.of(2016, Month.MARCH, 1, 23, 30, 00);
+		LocalDateTime comparestart5 = LocalDateTime.of(2016, Month.MARCH, 1, 7, 00, 00);
+		LocalDateTime compareend5 = LocalDateTime.of(2016, Month.MARCH, 1, 9, 00, 00);
+		LocalDateTime comparestart6 = LocalDateTime.of(2016, Month.MARCH, 1, 22, 00, 00);
+		LocalDateTime compareend6 = LocalDateTime.of(2016, Month.MARCH, 1, 23, 00, 00);
+		LocalDateTime comparestart7 = LocalDateTime.of(2016, Month.MARCH, 1, 8, 00, 00);
+		LocalDateTime compareend7 = LocalDateTime.of(2016, Month.MARCH, 1, 23, 00, 00);
 		
 		assertTrue(test.hasOverlap(start, end, comparestart1, compareend1)); //overlap: task tested same as compared task
 		assertTrue(test.hasOverlap(start, end, comparestart2, compareend2)); //overlap: task tested lies within compared task
@@ -121,6 +124,7 @@ public class TaskTest {
 	
 	//This fn test whether a task is overlapping w other task
 	//Equivalence partition: yes, no, no(not an event)
+	@Test
 	public void testIsOverlapping() {
 		ArrayList<String> _tags = new ArrayList<String>();
 		LocalDateTime notime = null;
