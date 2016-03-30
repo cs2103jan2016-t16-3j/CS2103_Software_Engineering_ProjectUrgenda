f352103299ea57569a623de5c0486695af6e1d0a
Tan Hui Kee Joanne <A0127358@u.nus.edu>
2016-03-30 22:24:06 +0800

    home junit test

---
+++ src/test/testCommand/HomeTest.java
@@ -0,0 +1,18 @@
+package testCommand;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import urgenda.command.Home;
+
+public class HomeTest {
+
+	//this fn is for testing if correct feedback msg is return for home command
+	@Test
+	public void test() {
+		Home tester = new Home();
+		assertEquals("Showing all tasks", tester.execute());
+	}
+
+}
