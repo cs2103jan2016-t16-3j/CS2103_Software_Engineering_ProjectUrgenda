d093adae3885f142fb73e494741ecc4aacc15fed
Koh Kim Wee <a0080436@u.nus.edu>
2016-03-30 19:26:19 +0800

    basic test across agenda

--- src/test/testUrgenda/RunTest.java
+++ src/test/testUrgenda/RunTest.java
@@ -2,17 +2,34 @@ package testUrgenda;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import urgenda.logic.Logic;
-import urgenda.logic.LogicData;
+import urgenda.util.StateFeedback;
+import urgenda.util.Task;
 
 public class RunTest {
+	
+	private static final String ADD_COMMAND = "add ";
+	private static final int ADD_INDEX = 0;
 
 	@Test
-	public void test() {
+	public void testAdd() {
+		String desc = "floating task";
+		
 		Logic logic = Logic.getInstance(true);
-		assertTrue(true);
+		StateFeedback actualOutput = logic.executeCommand(ADD_COMMAND + desc, ADD_INDEX);
+		Task newTask = new Task(desc);
+		ArrayList<Task> tasks = new ArrayList<Task>();
+		tasks.add(newTask);
+		StateFeedback expectedOutput = new StateFeedback(tasks, 0, 0, 1);
+		System.out.println(actualOutput.getAllTasks().getTasks());
+		System.out.println(tasks);
+		assertTrue(actualOutput.getAllTasks().getTasks().equals(tasks));
+		
+		
 	}
 
 }
