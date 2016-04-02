7ffe4bff54f76032575e47c8b79c0bc51720a319
Tan Hui Kee Joanne <A0127358@u.nus.edu>
2016-04-02 23:17:07 +0800

    minor edits

--- src/urgenda/logic/LogicSuggester.java
+++ src/urgenda/logic/LogicSuggester.java
@@ -22,8 +22,8 @@ public class LogicSuggester {
 	private static final String DONE_MESSAGE = "Mark selected task(s) as done";
 	
 	// TODO feedback for edit
-	private static final String EDIT = "<selected task> | [task no] [new desc] ";
-	private static final String EDIT_MESSAGE = "Edits description, date(s) and timing(s), and/or location of selected task";
+	private static final String EDIT = "(<selected task> | [task no])  ([new desc] | @[location] | -r/-rm (remove a time) | from/-s [start] | to/by/-e [end])";
+	private static final String EDIT_MESSAGE = "Edits desc, date(s) and timing(s), and/or location of selected task";
 	
 	private static final String SEARCH_TASK = "[desc] | [date/month/day/time] | [task type]";
 	private static final String SEARCH_MESSAGE = "Searches for task(s) displayed that matches the given input";
