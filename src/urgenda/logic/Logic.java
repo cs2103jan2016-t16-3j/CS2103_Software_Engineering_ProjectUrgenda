7cbb9f75ea0e705b9c5f9a9ab5ff4267f47462b6
Koh Kim Wee <a0080436@u.nus.edu>
2016-04-02 14:03:11 +0800

    added comments for logic

--- src/urgenda/logic/Logic.java
+++ src/urgenda/logic/Logic.java
@@ -16,27 +16,27 @@ import urgenda.util.SuggestFeedback;
 import urgenda.util.UrgendaLogger;
 
 public class Logic {
-	
+
 	private static final String MESSAGE_WELCOME = "Welcome to Urgenda!\nType \"demo\" for a simple demo, type \"help\" for the help menu.";
 	private static UrgendaLogger logger = UrgendaLogger.getInstance();
 	private static Logic _logic;
 	private LogicData _logicData;
 	private LogicCommand _logicCommand;
 	private LogicSuggester _logicSuggestion;
-	
+
 	// Default constructor for Logic
 	private Logic() {
 		_logicData = LogicData.getInstance();
 		_logicCommand = new LogicCommand();
 		_logicSuggestion = new LogicSuggester();
 	}
-	
+
 	private Logic(boolean isTest) {
 		_logicData = LogicData.getInstance(isTest);
 		_logicCommand = new LogicCommand();
 		_logicSuggestion = new LogicSuggester();
 	}
-	
+
 	// Implementation of Singleton pattern for Logic
 	public static Logic getInstance() {
 		if (_logic == null) {
@@ -46,7 +46,7 @@ public class Logic {
 		logger.getLogger().info("retrieving prev instance of logic");
 		return _logic;
 	}
-	
+
 	// alternative constructor for testing purposes
 	public static Logic getInstance(boolean isTest) {
 		if (_logic == null) {
@@ -54,47 +54,49 @@ public class Logic {
 		}
 		return _logic;
 	}
-	
+
 	/**
-	 * Executes the command given in string format, taking the relevant position if required
+	 * Executes the command given in string format, taking the relevant position
+	 * if required
 	 * 
 	 * @param command input string by the user
 	 * @param index current index pointed at by the user
-	 * @return StateFeedback which includes the current state of tasks as well as feedback line
+	 * @return StateFeedback which includes the current state of tasks as well
+	 *         as feedback line
 	 */
-	
 	public StateFeedback executeCommand(String command, int index) {
-		
+
 		logger.getLogger().info("executing user input: " + command);
-		
+
 		assert (index >= -1); // asserts that given index is non-negative OR -1(case when there is no tasks)
-		logger.getLogger().info("Checking index: " + index + " >= -1 " );
-		
+		logger.getLogger().info("Checking index: " + index + " >= -1 ");
+
 		// parser take in a string and return it in its corresponding class obj
-		Command currCmd = CommandParser.parseCommand(command,index);
+		Command currCmd = CommandParser.parseCommand(command, index);
 		assert (currCmd != null); // asserts that parser returns a command object
 		logger.getLogger().info("Checking cmd obj: " + currCmd + " is non null");
 
 		// To ensure that the command is applicable to the state
 		currCmd = checkAndFilterCommand(currCmd);
-		
+
 		String feedback;
 		// To update if there are any deadlines that turned overdue
 		_logicData.updateState();
 		feedback = _logicCommand.processCommand(currCmd);
-		
-		// To update after the command has been processed to ensure that the newly edited tasks are updated
+
+		// To update after the command has been processed to ensure that the
+		// newly edited tasks are updated
 		_logicData.updateState();
-		
-		// To check and change accordingly if the pointer is pointed towards an archived task 
-		// after the state is updated
+
+		// To check and change accordingly if the pointer is pointed towards an
+		// archived task after the state is updated
 		_logicData.checkPointer();
 		StateFeedback state = _logicData.getState();
 		state.setFeedback(feedback);
 		_logicData.saveContents();
 		return state;
 	}
-	
+
 	private Command checkAndFilterCommand(Command currCmd) {
 		if (_logicData.getCurrState() == LogicData.DisplayState.FIND_FREE) {
 			if (currCmd instanceof TaskCommand || currCmd instanceof ShowDetails) {
@@ -103,7 +105,7 @@ public class Logic {
 				} else {
 					currCmd = new Invalid(LogicData.DisplayState.FIND_FREE);
 				}
-				
+
 			}
 		} else if (_logicData.getCurrState() == LogicData.DisplayState.ARCHIVE) {
 			// TODO settle archive cases
@@ -111,7 +113,7 @@ public class Logic {
 				currCmd = new Invalid(LogicData.DisplayState.ARCHIVE);
 			}
 		}
-		
+
 		return currCmd;
 	}
 
@@ -120,20 +122,17 @@ public class Logic {
 	 * 
 	 * @return String containing the help manual
 	 */
-	
-	
 	public ArrayList<String> displayHelp() {
 		logger.getLogger().info("Help fn has been called");
-		
+
 		return _logicData.generateHelpManual();
 	}
-	
+
 	/**
 	 * Initialization of Logic upon launch of the program
 	 * 
 	 * @return StateFeedback containing the previously stored state
 	 */
-	
 	public StateFeedback retrieveStartupState() {
 		logger.getLogger().info("Retrieving prev launched info");
 		_logicData.clearOldArchive();
@@ -147,17 +146,22 @@ public class Logic {
 	 * 
 	 * @return String of location of current directory
 	 */
-	
 	public String getCurrentSaveDirectory() {
 		logger.getLogger().info("Retrieving current help directory");
 		return _logicData.retrieveCurrentDirectory();
 	}
-	
+
+	/**
+	 * Enable suggestion of the command while user is typing
+	 * 
+	 * @param currCmd current string that the user is typing
+	 * @return SuggestFeedback including strings of suggestions to user
+	 */
 	public SuggestFeedback getSuggestions(String currCmd) {
 		SuggestCommand suggCmd = CommandParser.parseCommandWord(currCmd);
 		return _logicSuggestion.processSuggestions(suggCmd);
 	}
-	
+
 	// clear storage for testing purposes
 	public void clearStorageTester() {
 		_logicData.reinitialiseStorageTester();
