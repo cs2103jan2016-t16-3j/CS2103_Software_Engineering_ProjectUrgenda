76769773afb9221c189e7e03c4691455b30992f2
Tan Hui Kee Joanne <A0127358@u.nus.edu>
2016-04-06 02:46:57 +0800

    refactor and javadocs comment

--- src/urgenda/command/Edit.java
+++ src/urgenda/command/Edit.java
@@ -22,14 +22,25 @@ public class Edit extends TaskCommand {
 	private LogicData _data;
 	private Integer _id;
 	private Task _prevTask;
-	private Task _temp; // a copy of prevTask for edition and comparison of time
-						// so that time in prevTask will not get modified during
-						// execution
+	// a copy of prevTask for edition and comparison of time so that time in
+	// prevTask will not get modified during execution
+	private Task _temp;
+	// new copy of task for edition w info for edition entered by user
 	private Task _newTask = new Task();
+	// a timing variable for storing of time that user entered w/o starttime and
+	// or endtime flag
 	LocalDateTime _unknown;
+	// a boolean variable to indicate removal of one timing (change of task type
+	// from event to deadline or deadline to floating
 	boolean isRemoveOnce = false;
+	// a boolean variable to indicate removal of a second timing (only set as
+	// true after isRemoveOnce is alr set as true)
+	// for change of task type from event to floating
 	boolean isRemoveTwice = false;
 
+	/**
+	 * Execute command of Edit which edits _prevTask to _newTask.
+	 */
 	public String execute() throws LogicException {
 		_data = LogicData.getInstance();
 		if (_id != null && _id.intValue() > -1) {
@@ -41,97 +52,167 @@ public class Edit extends TaskCommand {
 			logger.getLogger().severe("Exception(No edit match) thrown");
 			throw new LogicException(MESSAGE_NO_EDIT_MATCH);
 		} else {
-			if (_newTask.getSlot() == null || _newTask.getSlot().isEmpty()) {
-				_newTask.setSlot(null);
+			performEdition();
+			updateEdition();
+			String warning = checkValidity();
+			return taskMessageWithLocation(_prevTask) + MESSAGE_EDIT + taskMessageWithLocation(_newTask) + warning;
+		}
+	}
+
+	/*
+	 * check for warnings, start time after end time etc and set pointer to
+	 * edited task.
+	 */
+	private String checkValidity() throws LogicException {
+		String warning;
+		try {
+			checkTaskValidity(_newTask);
+			_data.deleteTask(_prevTask);
+			_data.addTask(_newTask);
+			_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
+			_data.setTaskPointer(_newTask);
+			_data.clearShowMoreTasks();
+			warning = checkPassed();
+			warning += findOverlaps();
+		} catch (LogicException e) {
+			logger.getLogger().severe("Exception occured: " + e);
+			_data.setCurrState(LogicData.DisplayState.INVALID_TASK);
+			// throws exception to prevent Edit being added to undo stack
+			throw new LogicException(MESSAGE_ERROR + e.getMessage());
+		}
+		return warning;
+	}
+
+	/*
+	 * update fields that are non-editable through edit command by users e.g.
+	 * multislot, priority, datemodified etc.
+	 */
+	private void updateEdition() {
+		_newTask.setId(_prevTask.getId());
+		_newTask.setSlot(_prevTask.getSlot());
+		_newTask.setIsCompleted(_prevTask.isCompleted());
+		_newTask.setIsImportant(_prevTask.isImportant());
+		_newTask.setDateAdded(_prevTask.getDateAdded());
+		_newTask.updateTaskType();
+		updateDateModified();
+	}
+
+	/*
+	 * perform edition of prev task to new task.
+	 */
+	private void performEdition() {
+		setMultiSlots();
+		removeTimings();
+		_temp.updateTaskType();
+		setDesc();
+		setLocation();
+		setNewTimings();
+	}
+
+	private void setNewTimings() {
+		if (_unknown == null) {
+			setDefiniteTime();
+		} else {
+			determineUnclearTimings();
+			setDefiniteTime();
+		}
+	}
+
+	// determine whether timing entered is for edition of start/end time if user
+	// input it w/o start &/ end flag.
+	private void determineUnclearTimings() {
+		switch (_temp.getTaskType()) {
+		case FLOATING: // Fallthrough
+		case DEADLINE:
+			// if prevtask is of type float/event (after removal of time had alr
+			// been performed), set unknown as endtime.
+			setUnknownAsEndTime();
+			break;
+		default:
+			// if prevTask is of type event, compare unknown w task time
+			checkStartEndAndSetUnknown();
+			break;
+		}
+	}
+
+	/*
+	 * if user entered new timing w/o start/end time, if task to be
+	 * edited(_temp) is of type event. Check if new timing entered is before
+	 * _temp start time, set new timing as starttime if new timing entered is
+	 * after _temp end time, set new timing as endtime. Else if is in btwn, take
+	 * default as changing start, set to start time
+	 */
+	private void checkStartEndAndSetUnknown() {
+		if (_newTask.getStartTime() == null && _newTask.getEndTime() == null) {
+			if (_unknown.isBefore(_temp.getStartTime())) {
+				_newTask.setStartTime(_unknown);
+			} else if (_unknown.isAfter(_temp.getEndTime())) {
+				_newTask.setEndTime(_unknown);
+			} else {
+				_newTask.setStartTime(_unknown);
 			}
-			if (isRemoveOnce) {
-				if (_temp.getStartTime() != null) {
-					_temp.setStartTime(null);
-				} else {
-					_temp.setEndTime(null);
-				}
+		} else { // when user enter 2 new time to be edited, one w flag one w/o
+					// e.g.
+			// 7pm to 10pm instead of from 7pm to 10pm, the unknown non-flag one
+			// could be determined
+			if (_newTask.getEndTime() == null) {
+				_newTask.setEndTime(_unknown);
+			} else {
+				_newTask.setStartTime(_unknown);
 			}
-			if (isRemoveTwice) {
+		}
+	}
+
+	private void setUnknownAsEndTime() {
+		if (_newTask.getStartTime() == null && _newTask.getEndTime() == null) {
+			_newTask.setEndTime(_unknown);
+		} else {
+			if (_newTask.getEndTime() == null) {
+				_newTask.setEndTime(_unknown);
+			} else {
+				_newTask.setStartTime(_unknown);
+			}
+		}
+	}
+
+	// when user enter timing to be edited w flag, set to _newTask accordingly
+	private void setDefiniteTime() {
+		if (_newTask.getStartTime() == null) {
+			_newTask.setStartTime(_temp.getStartTime());
+		}
+		if (_newTask.getEndTime() == null) {
+			_newTask.setEndTime(_temp.getEndTime());
+		}
+	}
+
+	private void setLocation() {
+		if (_newTask.getLocation() == null || _newTask.getLocation().equals("") && _prevTask.getLocation() != null) {
+			_newTask.setLocation(_prevTask.getLocation());
+		}
+	}
+
+	private void setDesc() {
+		if (_newTask.getDesc() == null || _newTask.getDesc().equals("") && _prevTask.getDesc() != null) {
+			_newTask.setDesc(_prevTask.getDesc());
+		}
+	}
+
+	private void setMultiSlots() {
+		if (_newTask.getSlot() == null || _newTask.getSlot().isEmpty()) {
+			_newTask.setSlot(null);
+		}
+	}
+
+	private void removeTimings() {
+		if (isRemoveOnce) {
+			if (_temp.getStartTime() != null) {
+				_temp.setStartTime(null);
+			} else {
 				_temp.setEndTime(null);
 			}
-			_temp.updateTaskType();
-			if (_newTask.getDesc() == null || _newTask.getDesc().equals("") && _prevTask.getDesc() != null) {
-				_newTask.setDesc(_prevTask.getDesc());
-			}
-			if (_newTask.getLocation() == null
-					|| _newTask.getLocation().equals("") && _prevTask.getLocation() != null) {
-				_newTask.setLocation(_prevTask.getLocation());
-			}
-			if (_unknown == null) {
-				if (_newTask.getStartTime() == null) {
-					_newTask.setStartTime(_temp.getStartTime());
-				}
-				if (_newTask.getEndTime() == null) {
-					_newTask.setEndTime(_temp.getEndTime());
-				}
-			} else {
-				switch (_temp.getTaskType()) {
-				case FLOATING: // Fallthrough
-				case DEADLINE:
-					if (_newTask.getStartTime() == null && _newTask.getEndTime() == null) {
-						_newTask.setEndTime(_unknown);
-					} else {
-						if (_newTask.getEndTime() == null) {
-							_newTask.setEndTime(_unknown);
-						} else {
-							_newTask.setStartTime(_unknown);
-						}
-					}
-					break;
-				default:
-					if (_newTask.getStartTime() == null && _newTask.getEndTime() == null) {
-						if (_unknown.isBefore(_temp.getStartTime())) {
-							_newTask.setStartTime(_unknown);
-						} else if (_unknown.isAfter(_temp.getEndTime())) {
-							_newTask.setEndTime(_unknown);
-						} else {
-							_newTask.setStartTime(_unknown);
-						}
-					} else {
-						if (_newTask.getEndTime() == null) {
-							_newTask.setEndTime(_unknown);
-						} else {
-							_newTask.setStartTime(_unknown);
-						}
-					}
-					break;
-				}
-				if (_newTask.getStartTime() == null) {
-					_newTask.setStartTime(_temp.getStartTime());
-				}
-				if (_newTask.getEndTime() == null) {
-					_newTask.setEndTime(_temp.getEndTime());
-				}
-			}
-			_newTask.setId(_prevTask.getId());
-			_newTask.setSlot(_prevTask.getSlot());
-			_newTask.setIsCompleted(_prevTask.isCompleted());
-			_newTask.setIsImportant(_prevTask.isImportant());
-			_newTask.setDateAdded(_prevTask.getDateAdded());
-			_newTask.updateTaskType();
-			updateDateModified();
-			String warning;
-			try {
-				checkTaskValidity(_newTask);
-				_data.deleteTask(_prevTask);
-				_data.addTask(_newTask);
-				_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
-				_data.setTaskPointer(_newTask);
-				_data.clearShowMoreTasks();
-				warning = checkPassed();
-				warning += findOverlaps();
-			} catch (LogicException e) {
-				logger.getLogger().severe("Exception occured: " + e);
-				_data.setCurrState(LogicData.DisplayState.INVALID_TASK);
-				// throws exception to prevent Edit being added to undo stack
-				throw new LogicException(MESSAGE_ERROR + e.getMessage());
-			}
-			return taskMessageWithLocation(_prevTask) + MESSAGE_EDIT + taskMessageWithLocation(_newTask) + warning;
+		}
+		if (isRemoveTwice) {
+			_temp.setEndTime(null);
 		}
 	}
 
@@ -139,11 +220,11 @@ public class Edit extends TaskCommand {
 		_newTask.setDateModified(LocalDateTime.now());
 		_prevTask.setDateModified(LocalDateTime.now());
 	}
-	
+
 	private String checkPassed() {
 		if (_newTask.getTaskType() == Task.Type.EVENT) {
 			if (_newTask.getEndTime().isBefore(LocalDateTime.now())) {
-				return MESSAGE_EVENT_PASSED;				
+				return MESSAGE_EVENT_PASSED;
 			}
 		} else if (_newTask.getTaskType() == Task.Type.DEADLINE) {
 			if (_newTask.getEndTime().isBefore(LocalDateTime.now())) {
@@ -156,7 +237,7 @@ public class Edit extends TaskCommand {
 	private String findOverlaps() {
 		ArrayList<Task> overlaps;
 		overlaps = _data.overlappingTasks(_newTask);
-		
+
 		if (overlaps.size() == 0) {
 			return "";
 		} else {
@@ -169,6 +250,10 @@ public class Edit extends TaskCommand {
 		}
 	}
 
+	/**
+	 * Undo method of Edit which undo the revert the task edited back to
+	 * original.
+	 */
 	public String undo() {
 		updateDateModified();
 		_data.deleteTask(_newTask);
@@ -177,6 +262,10 @@ public class Edit extends TaskCommand {
 		return taskMessageWithLocation(_newTask) + MESSAGE_REVERTED + taskMessageWithLocation(_prevTask);
 	}
 
+	/**
+	 * Redo method of Edit which redo the action and edit the task to the new
+	 * version.
+	 */
 	public String redo() {
 		updateDateModified();
 		_data.deleteTask(_prevTask);
@@ -185,22 +274,48 @@ public class Edit extends TaskCommand {
 		return taskMessageWithLocation(_prevTask) + MESSAGE_EDIT + taskMessageWithLocation(_newTask) + findOverlaps();
 	}
 
+	/**
+	 * Setter for the position of the task to be edited.
+	 * 
+	 * @param id
+	 */
 	public void setId(int id) {
 		_id = Integer.valueOf(id);
 	}
 
+	/**
+	 * Setter for the new edited task containing info of the fields to be edited
+	 * (e.g. new desc, location, timings etc).
+	 * 
+	 * @param newTask
+	 */
 	public void setNewTask(Task newTask) {
 		_newTask = newTask;
 	}
 
+	/**
+	 * Setter for unknown, a LocalDateTime variable for storing of timing that
+	 * user entered w/o starttime andor endtime flags.
+	 * 
+	 * @param unknown
+	 */
 	public void setUnknown(LocalDateTime unknown) {
 		_unknown = unknown;
 	}
 
+	/**
+	 * Setter for boolean variable isRemovedOnce which is used to indicate the
+	 * removal of one timing (change of task type).
+	 */
 	public void setIsRemovedOnce() {
 		isRemoveOnce = true;
 	}
 
+	/**
+	 * Setter for boolean variable isRemovedTwice (only set after isRemovedOnce
+	 * had alr been set as true) which is used to indicate the removal of two
+	 * timings.
+	 */
 	public void setIsRemovedTwice() {
 		isRemoveTwice = true;
 	}
--- src/urgenda/command/Search.java
+++ src/urgenda/command/Search.java
@@ -1,3 +1,4 @@
+//@@author A0127358Y
 package urgenda.command;
 
 import java.time.LocalDate;
@@ -10,6 +11,11 @@ import org.apache.commons.lang3.StringUtils;
 import urgenda.logic.LogicData;
 import urgenda.util.Task;
 
+/**
+ * 
+ * Search is the command object used for searching of tasks in Urgenda.
+ *
+ */
 public class Search extends Command {
 
 	private static final String MESSAGE_SHOWING = "Showing: ";
@@ -29,28 +35,74 @@ public class Search extends Command {
 	private Month _searchMonth;
 	private Integer _searchId;
 
-	// default constructor
+	/**
+	 * Default constructor for creating a new Search object.
+	 */
 	public Search() {
 
 	}
 
+	/**
+	 * Alternative constructor for Search command object with the String desc
+	 * for searching.
+	 * 
+	 * @param String
+	 *            input The desc entered by user for searching.
+	 */
 	public Search(String input) {
 		_searchDesc = input;
 	}
 
+	/**
+	 * Alternative constructor for Search command object with the LocalDate date
+	 * for searching.
+	 * 
+	 * @param LocalDate
+	 *            input The date entered by user for searching.
+	 */
 	public Search(LocalDate input) {
 		_searchDate = input;
 	}
 
+	/**
+	 * Alternative constructor for Search command object with the LocalDateTime
+	 * date&time for searching.
+	 * 
+	 * @param input
+	 *            The date and time entered by user for searching.
+	 */
 	public Search(LocalDateTime input) {
 		_searchDateTime = input;
 	}
 
+	/**
+	 * Alternative constructor for Search command object with the Month month
+	 * for searching.
+	 * 
+	 * @param input
+	 *            The month entered by user for searching of tasks falling on
+	 *            that month.
+	 */
 	public Search(Month input) {
 		_searchMonth = input;
 	}
 
-	// TODO: Further refactoring
+	/**
+	 * Alternative constructor for Search command object with the int id for
+	 * searching.
+	 * 
+	 * @param input
+	 *            The position entered by user for searching of a task (fn
+	 *            similarly as showmore).
+	 */
+	public Search(int input) {
+		_searchId = Integer.valueOf(input);
+	}
+
+	/**
+	 * Execute command of Search which searches for tasks in Urgenda that
+	 * matches the user specified input.
+	 */
 	public String execute() {
 		LogicData data = LogicData.getInstance();
 		ArrayList<Task> matches;
@@ -58,60 +110,7 @@ public class Search extends Command {
 		data.clearShowMoreTasks();
 
 		if (_searchDesc != null) {
-			String copy = _searchDesc; // copy of _searchDesc for modification,
-										// trimming and caseignore
-			int descCount = 0;
-			int typeCount = 0;
-			int nearMatchCount = 0;
-			switch (copy.toLowerCase().trim()) {
-			case "overdue":
-				matches = data.findMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				typeCount = findTypeOverdue(data, matches, typeCount);
-				break;
-			case "completed":
-				matches = data.findMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				typeCount = findTypeCompleted(data, matches, typeCount);
-				break;
-			case "important": // Fallthrough
-			case "impt": // Fallthrough
-			case "prioritise":
-				matches = data.findMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				typeCount = findTypePri(data, matches, typeCount);
-				break;
-			case "twotime": // Fallthrough
-			case "event":
-				matches = data.findMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				typeCount = findTypeEvent(data, matches, typeCount);
-				break;
-			case "onetime": // Fallthrough
-			case "duedate": // Fallthrough
-			case "deadline": // Fallthrough
-				matches = data.findMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				typeCount = findTypeDeadLine(data, matches, typeCount);
-				break;
-			case "floating": // Fallthrough
-			case "untimed":
-				matches = data.findMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				typeCount = findTypeFloat(data, matches, typeCount);
-				break;
-			case "archive":
-				matches = data.getArchives();
-				typeCount = matches.size();
-				break;
-			default:
-				matches = data.findRefinedMatchingDesc(_searchDesc);
-				descCount = matches.size();
-				nearMatchCount = findNearMatch(data, matches, nearMatchCount);
-				break;
-			}
-			feedback = generateSearchDescFeedback(data, matches, descCount, typeCount) 
-					+ getNearMatchFeedback(nearMatchCount, descCount, typeCount) ;
+			feedback = searchForDesc(data);
 
 		} else if (_searchDate != null) {
 			matches = data.findMatchingDates(_searchDate);
@@ -132,10 +131,71 @@ public class Search extends Command {
 		return feedback;
 	}
 
+	private String searchForDesc(LogicData data) {
+		ArrayList<Task> matches;
+		String feedback;
+		// copy of _searchDesc for modification, trimming and caseignore
+		String copy = _searchDesc;
+		int descCount = 0;
+		int typeCount = 0;
+		int nearMatchCount = 0;
+		switch (copy.toLowerCase().trim()) {
+		case "overdue":
+			matches = data.findMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			typeCount = findTypeOverdue(data, matches, typeCount);
+			break;
+		case "completed":
+			matches = data.findMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			typeCount = findTypeCompleted(data, matches, typeCount);
+			break;
+		case "important": // Fallthrough
+		case "impt": // Fallthrough
+		case "prioritise":
+			matches = data.findMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			typeCount = findTypePri(data, matches, typeCount);
+			break;
+		case "twotime": // Fallthrough
+		case "event":
+			matches = data.findMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			typeCount = findTypeEvent(data, matches, typeCount);
+			break;
+		case "onetime": // Fallthrough
+		case "duedate": // Fallthrough
+		case "deadline": // Fallthrough
+			matches = data.findMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			typeCount = findTypeDeadLine(data, matches, typeCount);
+			break;
+		case "floating": // Fallthrough
+		case "untimed":
+			matches = data.findMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			typeCount = findTypeFloat(data, matches, typeCount);
+			break;
+		case "archive":
+			matches = data.getArchives();
+			typeCount = matches.size();
+			break;
+		default:
+			matches = data.findRefinedMatchingDesc(_searchDesc);
+			descCount = matches.size();
+			nearMatchCount = findNearMatch(data, matches, nearMatchCount);
+			break;
+		}
+		feedback = generateSearchDescFeedback(data, matches, descCount, typeCount)
+				+ getNearMatchFeedback(nearMatchCount, descCount, typeCount);
+		return feedback;
+	}
+
 	private String getNearMatchFeedback(int nearMatchCount, int descCount, int typeCount) {
 		if (nearMatchCount != 0) {
 			if (descCount == 0 && typeCount == 0) {
-				return String.format(MESSAGE_SEARCH_NO_EXACT, _searchDesc) + MESSAGE_SHOWING + String.format(MESSAGE_NEAR_MATCH, nearMatchCount);
+				return String.format(MESSAGE_SEARCH_NO_EXACT, _searchDesc) + MESSAGE_SHOWING
+						+ String.format(MESSAGE_NEAR_MATCH, nearMatchCount);
 			} else {
 				return " and " + String.format(MESSAGE_NEAR_MATCH, nearMatchCount);
 			}
@@ -145,7 +205,7 @@ public class Search extends Command {
 
 	private int findNearMatch(LogicData data, ArrayList<Task> matches, int nearMatchCount) {
 		for (Task task : data.getDisplays()) {
-			if(StringUtils.getJaroWinklerDistance(_searchDesc, task.getDesc()) >= 0.8 && !matches.contains(task)) {
+			if (StringUtils.getJaroWinklerDistance(_searchDesc, task.getDesc()) >= 0.8 && !matches.contains(task)) {
 				matches.add(task);
 				nearMatchCount++;
 			}
@@ -256,7 +316,7 @@ public class Search extends Command {
 					+ String.format(MESSAGE_SEARCH_TYPE, _searchDesc);
 		} else if (descCount == 0 && typeCount != 0) {
 			feedback = MESSAGE_SHOWING + String.format(MESSAGE_SEARCH_TYPE, _searchDesc);
-		} else if (descCount != 0 && typeCount == 0){
+		} else if (descCount != 0 && typeCount == 0) {
 			feedback = MESSAGE_SHOWING + String.format(MESSAGE_SEARCH_DESC, _searchDesc);
 		} else {
 			feedback = "";
@@ -264,6 +324,9 @@ public class Search extends Command {
 		return feedback;
 	}
 
+	/*
+	 * find tasks that are of type floating
+	 */
 	private int findTypeFloat(LogicData data, ArrayList<Task> matches, int typeCount) {
 		for (Task task : data.getDisplays()) {
 			if (task.getTaskType().equals(Task.Type.FLOATING)) {
@@ -276,6 +339,9 @@ public class Search extends Command {
 		return typeCount;
 	}
 
+	/*
+	 * find tasks that are of type deadline
+	 */
 	private int findTypeDeadLine(LogicData data, ArrayList<Task> matches, int typeCount) {
 		for (Task task : data.getDisplays()) {
 			if (task.getTaskType().equals(Task.Type.DEADLINE)) {
@@ -288,6 +354,9 @@ public class Search extends Command {
 		return typeCount;
 	}
 
+	/*
+	 * find tasks that are of type event
+	 */
 	private int findTypeEvent(LogicData data, ArrayList<Task> matches, int typeCount) {
 		for (Task task : data.getDisplays()) {
 			if (task.getTaskType().equals(Task.Type.EVENT)) {
@@ -300,26 +369,33 @@ public class Search extends Command {
 		return typeCount;
 	}
 
+	/*
+	 * find tasks that are marked as important
+	 */
 	private int findTypePri(LogicData data, ArrayList<Task> matches, int typeCount) {
 		for (Task task : data.getDisplays()) {
 			if (task.isImportant()) {
 				typeCount++; // maintain count of all pri tasks
 			}
-			if (task.isImportant() && !matches.contains(task)) { // prevent adding duplicate copies which falls under
-																	//multi category eg. desc impt and isImpt
+			// prevent adding duplicate copies which falls under multi category
+			// eg. desc impt and isImpt
+			if (task.isImportant() && !matches.contains(task)) {
 				matches.add(task);
 			}
 		}
 		return typeCount;
 	}
 
+	/*
+	 * find tasks that are marked as done
+	 */
 	private int findTypeCompleted(LogicData data, ArrayList<Task> matches, int typeCount) {
 		for (Task task : data.getTaskList()) {
 			if (task.isCompleted()) {
 				typeCount++; // maintain count of all completed tasks
 			}
-			if (task.isCompleted() && !matches.contains(task)) { // prevent adding duplicate copies which falls under
-																//multi category 
+			// prevent adding duplicate copies which falls under multi category
+			if (task.isCompleted() && !matches.contains(task)) {
 				matches.add(task);
 			}
 		}
@@ -332,35 +408,70 @@ public class Search extends Command {
 		return typeCount;
 	}
 
+	/*
+	 * find tasks that are overdue
+	 */
 	private int findTypeOverdue(LogicData data, ArrayList<Task> matches, int typeCount) {
 		for (Task task : data.getDisplays()) {
 			if (task.isOverdue()) {
-				typeCount++; // get num of overdue tasks (including those which
-								// are both overdue & has desc overdue)
+				// get num of overdue tasks (including those which are both
+				// overdue & has desc overdue)
+				typeCount++;
 			}
-			if (task.isOverdue() && !matches.contains(task)) { // prevent adding of duplicate copies
+			// prevent adding of duplicate copies
+			if (task.isOverdue() && !matches.contains(task)) {
 				matches.add(task);
 			}
 		}
 		return typeCount;
 	}
 
+	/**
+	 * Setter for search by desc.
+	 * 
+	 * @param input
+	 *            The string desc used for searching.
+	 */
 	public void setSearchInput(String input) {
 		_searchDesc = input;
 	}
 
+	/**
+	 * Setter for search by date
+	 * 
+	 * @param input
+	 *            The LocalDate date used for searching.
+	 */
 	public void setSearchDate(LocalDate input) {
 		_searchDate = input;
 	}
 
+	/**
+	 * Setter for search by datetime
+	 * 
+	 * @param input
+	 *            The LocalDateTime dateandtime used for searching.
+	 */
 	public void setSearchDateTime(LocalDateTime input) {
 		_searchDateTime = input;
 	}
 
+	/**
+	 * Setter for search by month
+	 * 
+	 * @param input
+	 *            The Month month used for searching.
+	 */
 	public void setSearchMonth(Month input) {
 		_searchMonth = input;
 	}
 
+	/**
+	 * Setter for search by id
+	 * 
+	 * @param id
+	 *            The int position of task used for searching.
+	 */
 	public void setSearchId(int id) {
 		_searchId = Integer.valueOf(id);
 	}
