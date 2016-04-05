5478b050be7b95c98ad79f9bdb936e4b9e924ed7
Koh Kim Wee <a0080436@u.nus.edu>
2016-04-06 00:41:39 +0800

    refactored postpone

--- src/urgenda/command/Postpone.java
+++ src/urgenda/command/Postpone.java
@@ -1,3 +1,4 @@
+//@@author A008043
 package urgenda.command;
 
 import java.time.LocalDateTime;
@@ -8,9 +9,14 @@ import urgenda.util.MultipleSlot;
 import urgenda.util.Task;
 import urgenda.util.UrgendaLogger;
 
+/**
+ * Postpone command for postponing of tasks by the given timing.
+ *
+ */
 public class Postpone extends TaskCommand {
 
 	private static UrgendaLogger logger = UrgendaLogger.getInstance();
+
 	private static final String MESSAGE_NO_MATCH = "Invalid task number. No matches found to postpone";
 	private static final String MESSAGE_NO_TIME = "Invalid time entered";
 	private static final String MESSAGE_POSTPONE_FLOATING = "Task has no time to postpone";
@@ -21,12 +27,12 @@ public class Postpone extends TaskCommand {
 	private static final String MESSAGE_HOURS = " hour(s) ";
 	private static final String MESSAGE_MINUTES = " minute(s) ";
 	private static final String MESSAGE_SECONDS = " second(s) ";
-	
+
 	private Task _prevTask;
 	private Task _newTask;
 	private Integer _id;
 	private LogicData _data;
-	
+
 	private int _year;
 	private int _month;
 	private int _day;
@@ -34,6 +40,10 @@ public class Postpone extends TaskCommand {
 	private int _minute;
 	private int _second;
 
+	/**
+	 * Default Constructor for postpone which generates a command object that
+	 * postpones by 0.
+	 */
 	public Postpone() {
 		_year = 0;
 		_month = 0;
@@ -43,6 +53,23 @@ public class Postpone extends TaskCommand {
 		_second = 0;
 	}
 
+	/**
+	 * Constructor for assigning the timings for postponing based on the units
+	 * of time.
+	 * 
+	 * @param year
+	 *            Number of years to postpone.
+	 * @param month
+	 *            Number of months to postpone.
+	 * @param day
+	 *            Number of days to postpone.
+	 * @param hour
+	 *            Number of hours to postpone.
+	 * @param minute
+	 *            Number of minutes to postpone.
+	 * @param second
+	 *            Number of seconds to postpone.
+	 */
 	public Postpone(int year, int month, int day, int hour, int minute, int second) {
 		_year = year;
 		_month = month;
@@ -52,11 +79,44 @@ public class Postpone extends TaskCommand {
 		_second = second;
 	}
 
+	/**
+	 * Execution method of the Postpone command to postpone the task by the
+	 * given duration.
+	 */
 	public String execute() throws LogicException {
 		_data = LogicData.getInstance();
-		if (_id != null && _id.intValue() != -1) {
-			_prevTask = _data.findMatchingPosition(_id.intValue());
-		}
+		findTaskToPostpone();
+		checkValidPostponeTask();
+		createPostponedTask();
+		replacePostponedTask();
+		return generateFeedback();
+	}
+
+	/*
+	 * Updates the state of Urgenda with the new postponed task.
+	 */
+	private void replacePostponedTask() {
+		_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
+		_data.deleteTask(_prevTask);
+		_data.addTask(_newTask);
+		_data.setTaskPointer(_newTask);
+		_data.clearShowMoreTasks();
+	}
+
+	/*
+	 * Creates a new task based on the previous task and adds the new time.
+	 */
+	private void createPostponedTask() {
+		_prevTask.setDateModified(LocalDateTime.now());
+		_newTask = new Task(_prevTask);
+		checkSlots();
+		addTime(_newTask);
+	}
+
+	/*
+	 * Checks if the task/postpone timing is valid.
+	 */
+	private void checkValidPostponeTask() throws LogicException {
 		if (!isValidPostpone()) {
 			_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
 			logger.getLogger().severe("Exception no postpone time thrown");
@@ -65,39 +125,46 @@ public class Postpone extends TaskCommand {
 			_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
 			logger.getLogger().severe("Exception(No postpone match) thrown");
 			throw new LogicException(MESSAGE_NO_MATCH);
-		} else if (_prevTask.getTaskType() == Task.Type.FLOATING){
+		} else if (_prevTask.getTaskType() == Task.Type.FLOATING) {
 			_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
 			logger.getLogger().severe("Exception postpone of floating thrown");
 			throw new LogicException(MESSAGE_POSTPONE_FLOATING);
-		} else {
-			_prevTask.setDateModified(LocalDateTime.now());
-			_newTask = new Task(_prevTask);
-			checkSlots();
-			addTime(_newTask);
-			_data.setCurrState(LogicData.DisplayState.ALL_TASKS);
-			_data.deleteTask(_prevTask);
-			_data.addTask(_newTask);
-			_data.setTaskPointer(_newTask);
-			_data.clearShowMoreTasks();
 		}
-		return generateFeedback();
 	}
 
+	/*
+	 * Finds the task to postpone based on the given index.
+	 */
+	private void findTaskToPostpone() {
+		if (_id != null && _id.intValue() != -1) {
+			_prevTask = _data.findMatchingPosition(_id.intValue());
+		}
+	}
+
+	/*
+	 * Checks if postpone timing is valid, if all attributes are empty.
+	 */
 	private boolean isValidPostpone() {
 		if (_year == 0 && _month == 0 && _day == 0 && _hour == 0 && _minute == 0 && _second == 0) {
 			return false;
 		} else {
-			return true;			
+			return true;
 		}
 	}
 
+	/*
+	 * Creates a new slot if the previous task has a slot.
+	 */
 	private void checkSlots() {
 		if (_prevTask.getSlot() != null) {
 			_newTask.setSlot(new MultipleSlot(_prevTask.getSlot()));
 		}
-		
+
 	}
 
+	/*
+	 * Appends the feedback string of the different durations.
+	 */
 	private String generateFeedback() {
 		String feedback = String.format(MESSAGE_POSTPONED_TASK, _prevTask.getDesc());
 		if (_year > 0) {
@@ -121,28 +188,34 @@ public class Postpone extends TaskCommand {
 		return feedback;
 	}
 
+	/*
+	 * Adds timing to the task based on the task type.
+	 */
 	private void addTime(Task task) {
 		if (task.getTaskType() == Task.Type.EVENT) {
 			LocalDateTime start = task.getStartTime();
-			start = start.plusYears(_year);
-			start = start.plusMonths(_month);
-			start = start.plusDays(_day);
-			start = start.plusHours(_hour);
-			start = start.plusMinutes(_minute);
-			start = start.plusSeconds(_second);
+			start = addTime(start);
 			task.setStartTime(start);
-		}		
-
+		}
 		LocalDateTime end = task.getEndTime();
-		end = end.plusYears(_year);
-		end = end.plusMonths(_month);
-		end = end.plusDays(_day);
-		end = end.plusHours(_hour);
-		end = end.plusMinutes(_minute);
-		end = end.plusSeconds(_second);
+		end = addTime(end);
 		task.setEndTime(end);
 	}
 
+	private LocalDateTime addTime(LocalDateTime time) {
+		time = time.plusYears(_year);
+		time = time.plusMonths(_month);
+		time = time.plusDays(_day);
+		time = time.plusHours(_hour);
+		time = time.plusMinutes(_minute);
+		time = time.plusSeconds(_second);
+		return time;
+	}
+
+	/**
+	 * Undo method for postpone where the postponed task is replaced back with
+	 * the original task.
+	 */
 	public String undo() {
 		_prevTask.setDateModified(LocalDateTime.now());
 		_newTask.setDateModified(LocalDateTime.now());
@@ -153,29 +226,10 @@ public class Postpone extends TaskCommand {
 		return generateFeedback();
 	}
 
-	// TODO remove if not needed
-	private void minusTime() {
-		if (_prevTask.getTaskType() == Task.Type.EVENT) {
-			LocalDateTime start = _prevTask.getStartTime();
-			start = start.minusYears(_year);
-			start = start.minusMonths(_month);
-			start = start.minusDays(_day);
-			start = start.minusHours(_hour);
-			start = start.minusMinutes(_minute);
-			start = start.minusSeconds(_second);
-			_prevTask.setStartTime(start);
-		}		
-
-		LocalDateTime end = _prevTask.getEndTime();
-		end = end.minusYears(_year);
-		end = end.minusMonths(_month);
-		end = end.minusDays(_day);
-		end = end.minusHours(_hour);
-		end = end.minusMinutes(_minute);
-		end = end.minusSeconds(_second);
-		_prevTask.setEndTime(end);
-	}
-
+	/**
+	 * Redo method for postpone where the postponed task is added back and
+	 * replaces the original task.
+	 */
 	public String redo() {
 		_prevTask.setDateModified(LocalDateTime.now());
 		_newTask.setDateModified(LocalDateTime.now());
@@ -186,42 +240,84 @@ public class Postpone extends TaskCommand {
 		return generateFeedback();
 	}
 
+	/**
+	 * Setter for the Year duration.
+	 * 
+	 * @param year
+	 *            Number of years to postpone.
+	 */
 	public void setYear(int year) {
 		if (year >= 0) {
-			_year = year;			
+			_year = year;
 		}
 	}
 
+	/**
+	 * Setter for the month duration.
+	 * 
+	 * @param year
+	 *            Number of months to postpone.
+	 */
 	public void setMonth(int month) {
 		if (month >= 0) {
-			_month = month;			
+			_month = month;
 		}
 	}
 
+	/**
+	 * Setter for the day duration.
+	 * 
+	 * @param year
+	 *            Number of days to postpone.
+	 */
 	public void setDay(int day) {
 		if (day >= 0) {
-			_day = day;			
+			_day = day;
 		}
 	}
 
+	/**
+	 * Setter for the hour duration.
+	 * 
+	 * @param year
+	 *            Number of hours to postpone.
+	 */
 	public void setHour(int hour) {
 		if (hour >= 0) {
-			_hour = hour;			
+			_hour = hour;
 		}
 	}
 
+	/**
+	 * Setter for the minute duration.
+	 * 
+	 * @param year
+	 *            Number of minutes to postpone.
+	 */
 	public void setMinute(int minute) {
 		if (minute >= 0) {
-			_minute = minute;			
+			_minute = minute;
 		}
 	}
 
+	/**
+	 * Setter for the second duration.
+	 * 
+	 * @param year
+	 *            Number of seconds to postpone.
+	 */
 	public void setSecond(int second) {
 		if (second >= 0) {
-			_second = second;			
+			_second = second;
 		}
 	}
-	
+
+	/**
+	 * Setter for the Id of the task to be postponed.
+	 * 
+	 * @param id
+	 *            task position to be postponed.
+	 */
 	public void setId(int id) {
 		_id = Integer.valueOf(id);
 	}
